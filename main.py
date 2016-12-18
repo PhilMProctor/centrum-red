a887447aafaf11dbacd57eef0e5c666a5395147f
pmpdev <phil.m.proctor@gmail.com>
2016-12-18 22:11:39 +0000

    Updated templates

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import jinja2
 # import Tornado
 import tornado.ioloop
 import tornado.web
+import bcrypt
+import unicodedata
 
 # Controller Config
 sPORT ='/dev/ttyACM0'
@@ -22,135 +24,163 @@ jinja_environment = \
     jinja2.Environment(autoescape=True, extensions=['jinja2.ext.autoescape'], loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
 
 class BaseHandler(tornado.web.RequestHandler):
-	def jinja2(self):
-		return jinja2.get_jinja2(app=self.app)
+    def jinja2(self):
+        return jinja2.get_jinja2(app=self.app)
 
-	def render_template(
-		self,
-		filename,
-		template_values,
-		**template_args
-		):
-		template = jinja_environment.get_template(filename)
-		self.write(template.render(template_values))
+    def render_template(
+        self,
+        filename,
+        template_values,
+        **template_args
+        ):
+        template = jinja_environment.get_template(filename)
+        self.write(template.render(template_values))
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("hms")
+        if not user_id: return None
+        return models.conn.execute("SELECT * FROM users WHERE id = %s", int(user_id))
 
 # Main page Handlers
 
 # Main Page
 class MainHandler(BaseHandler):
-	def get(self):
-		switch_config = models.conn.execute("SELECT name from switch_settings where active = 0")
-		power = 1
-		params = {
-			'switch_config': switch_config,
-			'power': power
-			}
-		self.render_template('index.html', params)
+    @tornado.web.authenticated
+    def get(self):
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.write("Hello, " + name)
+        self.render_template('index.html', params)
+
+class AuthLoginHandler(BaseHandler):
+    def get (self):
+        error = ""
+        params = {
+        'error': error
+        }
+        self.render_template('login.html', params)
+
+    def post(self):
+        admin = models.conn.execute("SELECT * FROM users WHERE username = %s",
+            self.get_argument("username"))
+        if not admin:
+            self.render_template("login.html", error="login error")
+        return
+        hashed_password = yield executor.submit(
+            bcrypt.hashpw, tornado.escape.utf8(self.get_argument("password")),
+            tornado.escape.utf8(admin.hashed_password))
+        if hashed_password == admin.hashed_password:
+            self.set_secure_cookie("hms", str(author.id))
+            self.redirect(self.get_argument("next", "/"))
+        else:
+            self.render_template("login.html", error="incorrect password")
+		
+		
 
 # Lights Page
 class LightsHandler(BaseHandler):
-	def get(self):
-		#conn = sqlite3.connect('/home/phil/git/Centrum/db/centrum.db')
-		cursor = models.conn.execute("SELECT id,\
-			name, description, location, switch_on,\
-			switch_off, state from switch_settings where cat='Light' and state = 1")
-		params = {
-		'cursor': cursor,
-		}
-		self.render_template('lights.html', params)
-
-	def post(self):
-		switch_ctl = self.get_argument('switch_action')
-		ser = serial.Serial(sPORT, 9600)
-		time.sleep(1.5)
-		ser.write(switch_ctl)
-		ser.close()
-		self.redirect('/lights')
+    def get(self):
+        cursor = models.conn.execute("SELECT id,\
+        name, description, location, switch_on,\
+        switch_off, state from switch_settings where cat='Light' and state = 1")
+        params = {
+        'cursor': cursor,
+        }
+        self.render_template('lights.html', params)
+    def post(self):
+        switch_ctl = self.get_argument('switch_action')
+        ser = serial.Serial(sPORT, 9600)
+        time.sleep(1.5)
+        ser.write(switch_ctl)
+        ser.close()
+        self.redirect('/lights')
 
 # Power Page
 class PowerHandler(BaseHandler):
-	def get(self):
-		cursor = models.conn.execute("SELECT id,\
-			name, description, location, switch_on,\
-			switch_off, state from switch_settings where cat='Power' and state = 1")
-		params = {
-		'cursor': cursor,
-		}
-		self.render_template('power.html', params)
+    def get(self):
+        cursor = models.conn.execute("SELECT id,\
+            name, description, location, switch_on,\
+            switch_off, state from switch_settings where cat='Power' and state = 1")
+        params = {
+        'cursor': cursor,
+        }
+        self.render_template('power.html', params)
 
-	def post(self):
-		switch_ctl = self.get_argument('switch_action')
-		ser = serial.Serial(sPORT, 9600)
-		time.sleep(1.5)
-		ser.write(switch_ctl)
-		ser.close()
-		self.redirect('/power')
+    def post(self):
+        switch_ctl = self.get_argument('switch_action')
+        ser = serial.Serial(sPORT, 9600)
+        time.sleep(1.5)
+        ser.write(switch_ctl)
+        ser.close()
+        self.redirect('/power')
 
 # Monitor Page
 class MonitorHandler(BaseHandler):
-	def get(self):
-		active_home = ""
-		active_settings = ""
-		params = {
-		'active_home': active_home,
-		'active_settings': active_settings
-		}
-		self.render_template('monitor.html', params)
+    def get(self):
+        active_home = ""
+        active_settings = ""
+        params = {
+        'active_home': active_home,
+        'active_settings': active_settings
+        }
+        self.render_template('monitor.html', params)
 
 		# Monitor Page
 class SwitchSetHandler(BaseHandler):
-	def get(self):
+    def get(self):
 		# Extracts the switch values <switch_vals> from the database and loads the page
-		switchset = models.conn.execute("SELECT id,\
-			name, description, location, cat, switch_on,\
-			switch_off, active from switch_settings where cat='Light'")
-		params = {
-		'switchset': switchset,
-		}
-		self.render_template('switchset.html', params)
+        switchset = models.conn.execute("SELECT id,\
+            name, description, location, cat, switch_on,\
+            switch_off, active from switch_settings where cat='Light'")
+        params = {
+        'switchset': switchset,
+        }
+        self.render_template('switchset.html', params)
 
-	def post(self):
+    def post(self):
 		# Posts the switch ID to the SwitchEditHandler class via the edit page
-		switch_ctl = self.get_argument('switchID')
-		self.redirect('/sEdit' + '?switchID=' + switch_ctl)
+        switch_ctl = self.get_argument('switchID')
+        self.redirect('/sEdit' + '?switchID=' + switch_ctl)
 
 class SwitchEditHandler(BaseHandler):
-	def get(self):
+    def get(self):
 		# Selects the switch details from the database which have the ID of <switch_ctl>
-		switch_ctl = self.get_argument('switchID')
-		switchVals = models.conn.execute("SELECT * from switch_settings where id = %s" %switch_ctl)
-		params = {
-		'switchVals': switchVals,
-		}
-		self.render_template('edit.html', params)
+        switch_ctl = self.get_argument('switchID')
+        switchVals = models.conn.execute("SELECT * from switch_settings where id = %s" %switch_ctl)
+        params = {
+        'switchVals': switchVals,
+        }
+        self.render_template('edit.html', params)
 
-	def post(self):
+    def post(self):
 		# Builds a update statement for sqlite
-		switchID = self.get_argument('switchID')
-		switchName = self.get_argument('switchName')
-		switchDesc = self.get_argument('description')
-		switchLoc = self.get_argument('location')
-		switchCat = self.get_argument('cat')
-		switchSetNum = self.get_argument('switchNum')
-		switchSetRnum = self.get_argument('switchRnum')
-		switchOn = self.get_argument('switchOn')
-		switchOff = self.get_argument('switchOff')
-		#switchActive = self.get_argument('active')
+        switchID = self.get_argument('switchID')
+        switchName = self.get_argument('switchName')
+        switchDesc = self.get_argument('description')
+        switchLoc = self.get_argument('location')
+        switchCat = self.get_argument('cat')
+        switchSetNum = self.get_argument('switchNum')
+        switchSetRnum = self.get_argument('switchRnum')
+        switchOn = self.get_argument('switchOn')
+        switchOff = self.get_argument('switchOff')
+        #switchActive = self.get_argument('active')
 		#switchPower = self.get_argument('power')
-		cursor = models.conn.execute("UPDATE switch_settings SET name = %s, description = %s, \
-			location = %s, cat = %s, setting_num = %s, setting_rnum = %s WHERE id = %s" % \
-			("'" + switchName + "'", "'"+switchDesc+"'", "'"+switchLoc+"'", "'"+switchCat+"'", "'"+switchSetNum+"'", "'"+switchSetRnum+"'", "'"+switchID+"'"))
-		models.conn.commit()
-		self.redirect(switchCat)
+        cursor = models.conn.execute("UPDATE switch_settings SET name = %s, description = %s, \
+            location = %s, cat = %s, setting_num = %s, setting_rnum = %s WHERE id = %s" % \
+            ("'" + switchName + "'", "'"+switchDesc+"'", "'"+switchLoc+"'", "'"+switchCat+"'", "'"+switchSetNum+"'", "'"+switchSetRnum+"'", "'"+switchID+"'"))
+        models.conn.commit()
+        self.redirect(switchCat)
 
-settings = {'debug': True}
+settings = {'debug': True,
+'cookie_secret':"fesdkodkokewqkdwqpkd",
+'login_url':'/login'}
 
 handlers = [(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path}),
-            (r'/', MainHandler),
-            (r'/lights', LightsHandler),
-            (r'/power', PowerHandler),
-			(r'/switchset', SwitchSetHandler),
-			(r'/sEdit', SwitchEditHandler),
+    (r'/', MainHandler),
+    (r'/lights', LightsHandler),
+    (r'/power', PowerHandler),
+    (r'/switchset', SwitchSetHandler),
+    (r'/sEdit', SwitchEditHandler),
+    (r'/login', AuthLoginHandler),
 ]
 
 application = tornado.web.Application(handlers, **settings)
--- models.py
+++ models.py
@@ -4,4 +4,4 @@ import sqlite3
 db_loc = '../db/centrum.db'
 
 #conn = sqlite3.connect('/home/cabox/workspace/db/centrum.db')
-conn = sqlite3.connect('/home/phil/git/Centrum/centrum-red/db/centrum.db')
+conn = sqlite3.connect('/home/phil/git/centrum-red/db/centrum.db')
